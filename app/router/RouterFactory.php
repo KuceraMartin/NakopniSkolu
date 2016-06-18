eaff9d5ade5cf6d9a8cdb9800b53a7e5739321c4
krekoun <krekoun@gmail.com>
2016-06-18 23:02:03 +0200

    created new Router

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -3,23 +3,36 @@
 namespace App;
 
 use Nette;
+use Nette\Application\IRouter;
 use Nette\Application\Routers\RouteList;
 use Nette\Application\Routers\Route;
-use Nette\Application\Routers\SimpleRouter;
 
 
 class RouterFactory
 {
 
     /**
-     * @return Nette\Application\IRouter
+     * @return IRouter
      */
     public static function createRouter()
     {
         $router = new RouteList;
-        $router[] = new Route('', 'Front:Homepage:default');
-        $router[] = new Route('admin', 'Admin:Dashboard:default');
-        $router[] = new SimpleRouter();
+
+        //Admin
+        $router[] = new Route('admin/<presenter>/<action>/<id>', [
+            'module' => 'Admin',
+            'presenter' => 'Dashboard',
+            'action' => 'default',
+            'id' => NULL
+        ]);
+
+        //Front
+        $router[] = new Route('<presenter>/<action>/<id>', [
+            'module' => 'Front',
+            'presenter' => 'Homepage',
+            'action' => 'default',
+            'id' => NULL
+        ]);
 
         return $router;
     }
