49d37230659faad82e137a130b7d491e442af90c
Martin Kučera <m.spidy@gmail.com>
2016-06-19 01:06:25 +0200

    basic dashboard

--- app/AdminModule/presenters/DashboardPresenter.php
+++ app/AdminModule/presenters/DashboardPresenter.php
@@ -3,7 +3,17 @@
 namespace App\AdminModule\Presenters;
 
 
+use App\Model\ProjectsRepository;
+
 class DashboardPresenter extends SecuredPresenter
 {
+
+	/** @var ProjectsRepository @inject */
+	public $projects;
+
+	public function renderDefault()
+	{
+		$this->template->projects = $this->projects->findBy(['school' => $this->user->id]);
+	}
 
 }
